81e1a12730dd5898207e38c4ee56f881f12531df
brit-starks <brittneytechiethomas@gmail.com>
2019-09-13 21:00:04 -0700

    Make API request, add state

---
+++ client/src/components/PlayerCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PlayerCard = (props) => {
+  console.log('PlayerCard.js: PROPS', props);
+
+  return(
+
+    <div className='player-wrapper'>
+
+    </div>
+  )
+}
+
+export default PlayerCard;
