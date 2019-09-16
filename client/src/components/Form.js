4b1b40b4dfc19020e9c868da4ef1845ea50b3b53
brit-starks <brittneytechiethomas@gmail.com>
2019-09-16 10:42:55 -0700

    Add event handlers

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -2,13 +2,26 @@ import React, { useState } from 'react';
 import './form.scss';
 
 const Form = () => {
+  const [search, setSearch] = useState([]);
   const [fetchInput, setFetchInput] = useState([]);
 
+
+  const handleChange = e => {
+    setSearch(e.target.value);
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setFetchInput(`http://localhost:5000/api/players`)
+  }
+
   return(
-  <form className='form'>
+  <form className='form' submit={handleSubmit}>
     <input
       type='text'
-      value=''
+      name='search'
+      value={fetchInput}
+      onChange={handleChange}
       placeholder='Search player'
     />
   </form>
---
+++ client/src/components/useForm.js
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+
+export const useForm = (initialValue, cb) => {
+  // const handleChange = e => {
+  //   setFetchInput(e.target.value);
+  // }
+}
